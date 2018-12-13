bd0556721c5df3ed6728e73893057f5346770f34
limseongeun <lseeee@naver.com>
2018-12-13 17:06:59 +0900

    Updates

---
+++ precache-manifest.be306c553dca6b751b0d5fac29100c22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c2892235c8502b7ddea9",
+    "url": "/movie_app/static/js/main.c2892235.chunk.js"
+  },
+  {
+    "revision": "769e228087a92a571f8d",
+    "url": "/movie_app/static/js/1.769e2280.chunk.js"
+  },
+  {
+    "revision": "c2892235c8502b7ddea9",
+    "url": "/movie_app/static/css/main.25ba4668.chunk.css"
+  },
+  {
+    "revision": "db46ed72ef7dcf31ee3c007658d7c1b5",
+    "url": "/movie_app/index.html"
+  }
+];
