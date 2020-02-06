58d9b461640d2ee234cdd156a99fa09766372f25
limseongeun <lseeee@naver.com>
2020-02-06 16:32:27 +0900

    update

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
   }
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
    .then(response => response.json())
    .then(json => json.data.movies)
    .catch(err => console.log(err))
